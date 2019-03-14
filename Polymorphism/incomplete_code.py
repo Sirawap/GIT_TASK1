cf82b2f74100fbd565dd59175ec610f336b640a3
SPVJ <42379752+SPVJ@users.noreply.github.com>
2019-03-14 17:49:57 +0700

    Update incomplete_code.py

--- Polymorphism/incomplete_code.py
+++ Polymorphism/incomplete_code.py
@@ -21,17 +21,25 @@ class Walk( Transportation ):
    def find_cost( self ):
       return 0
 
+class Uber(Transportation):
+
+   def __init__(self,start,end,distance):
+      super().__init__(start,end,distance)
+
+   def find_cost( self ):
+      return self.distance * 50
+
 
    
-# main program
+if __name__ =="__main__":
 
-travel_cost = 0
+   travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+   #trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
          #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
          #Train("Ladkrabang Station","Payathai Station",40,6),
          #Taxi("Payathai Station","The British Council",3) ]
 
-for travel in trip:
-   travel_cost += travel.find_cost()
-print travel_cost
+   #for travel in trip:
+   #  travel_cost += travel.find_cost()
+   #print travel_cost
